b71ae4af6dfcf09d69f02de03ffaca173b37b7d7
minhoslucas <lucasbeserrafernandes@gmail.com>
2025-06-23 23:52:14 -0300

    mais comentarios

--- src/test/java/GeneralTests.java
+++ src/test/java/GeneralTests.java
@@ -11,12 +11,18 @@ import org.junit.jupiter.api.Test;
 
 public class GeneralTests {
 
+    /**
+     * Configura o ambiente necessário antes da execução dos testes.
+     */
     @BeforeAll
     static void beforeAll(){
         SharedConstants.createGameVersion();
         Bootstrap.initialize();
     }
 
+    /**
+     * Testa a criação de um ItemStack com uma quantidade específica do item.
+     */
     @Test
     void TestItemCreation(){
         ItemStack item = new ItemStack(ModItems.SANTIAGUITA_INGOT, 3);
@@ -26,17 +32,23 @@ public class GeneralTests {
         Assertions.assertEquals(3, item.getCount());
     }
 
+    /**
+     * Testa as propriedades específicas de um ItemStack, verificando componentes de dados.
+     */
     @Test
     void TestItemProperties(){
         ItemStack item = new ItemStack(ModItems.STRIKE_TOTEM, 1);
         Assertions.assertEquals(item.getItem().getComponents().get(DataComponentTypes.DEATH_PROTECTION), DeathProtectionComponent.TOTEM_OF_UNDYING);
     }
 
+    /**
+     * Testa a criação de um ItemStack a partir de um bloco e a contagem correta.
+     */
     @Test
     void TestBlockCreation(){
         ItemStack item = new ItemStack(ModBlocks.SANTIAGUITA_BLOCK, 2);
         Assertions.assertTrue(item.isOf(ModBlocks.SANTIAGUITA_BLOCK.asItem()));
         Assertions.assertEquals(2, item.getCount());
     }
-
 }
+
