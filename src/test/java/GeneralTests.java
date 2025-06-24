7053b298efda27d1eaf81f43ef800f59e73591c9
minhoslucas <lucasbeserrafernandes@gmail.com>
2025-06-23 23:14:40 -0300

    testes unitários

---
+++ src/test/java/GeneralTests.java
@@ -0,0 +1,42 @@
+import net.minecraft.Bootstrap;
+import net.minecraft.SharedConstants;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.DeathProtectionComponent;
+import net.minecraft.item.ItemStack;
+import org.amemeida.santiago.registry.blocks.ModBlocks;
+import org.amemeida.santiago.registry.items.ModItems;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class GeneralTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        SharedConstants.createGameVersion();
+        Bootstrap.initialize();
+    }
+
+    @Test
+    void TestItemCreation(){
+        ItemStack item = new ItemStack(ModItems.SANTIAGUITA_INGOT, 3);
+
+        Assertions.assertNotNull(item);
+        Assertions.assertTrue(item.isOf(ModItems.SANTIAGUITA_INGOT));
+        Assertions.assertEquals(3, item.getCount());
+    }
+
+    @Test
+    void TestItemProperties(){
+        ItemStack item = new ItemStack(ModItems.STRIKE_TOTEM, 1);
+        Assertions.assertEquals(item.getItem().getComponents().get(DataComponentTypes.DEATH_PROTECTION), DeathProtectionComponent.TOTEM_OF_UNDYING);
+    }
+
+    @Test
+    void TestBlockCreation(){
+        ItemStack item = new ItemStack(ModBlocks.SANTIAGUITA_BLOCK, 2);
+        Assertions.assertTrue(item.isOf(ModBlocks.SANTIAGUITA_BLOCK.asItem()));
+        Assertions.assertEquals(2, item.getCount());
+    }
+
+}
